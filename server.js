532d58eacd17372639684c929fb0cd88b85e1491
Vlad Chernenko <vladchernenko@protonmail.com>
2022-06-10 23:34:04 +0300

    License changes

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 import UWS from 'uWebSockets.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
 
-UWS.App().get('/',a=>a.end('Hello')).listen(4444,ok=>{})
+
+
+let CONFIGS=JSON.parse(fs.readFileSync('config.json'))
+
+
+//_______________________________________________________ START _______________________________________________________
+
+UWS.App().get('/',a=>{
+    
+    a.onAborted(()=>a.aborted=true)
+
+    a.end(fs.readFileSync('Parking(scanner).html').toString('utf-8'))
+    
+    // fetch(`${CONFIGS.SOURCE}/i`).then(r=>r.json()).then(val=>
+        
+    //     a.end(JSON.stringify(val,null,2))
+        
+    // ).catch(e=>a.end('Pingback failed.Node is sleeping'))
+   
+
+}).listen(4444,ok=>{})
